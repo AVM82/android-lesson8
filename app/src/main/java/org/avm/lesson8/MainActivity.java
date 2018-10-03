ca2d60b9abd20ba8c1fd5faddb161e6cc31ccbfa
AVM82 <1982AlexVM@gmail.com>
2018-10-03 13:52:44 +0300

    add bottom menu to change the background color on fragment

--- app/src/main/java/org/avm/lesson8/MainActivity.java
+++ app/src/main/java/org/avm/lesson8/MainActivity.java
@@ -1,13 +1,15 @@
 package org.avm.lesson8;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.FragmentListener {
 
@@ -41,10 +43,13 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Frag
         item.setChecked(!item.isChecked());
         FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
         mFragmentTransaction.setCustomAnimations(R.anim.slide_from_top, R.anim.disappear);
+        mFragmentTransaction.setTransition(FragmentTransaction.TRANSIT_ENTER_MASK);
         switch (item.getItemId()) {
             case R.id.first_fragment: {
                 if (item.isChecked()) {
-                    mFragmentTransaction.add(R.id.container1, mFragment1).commit();
+                    mFragmentTransaction.add(R.id.container1, mFragment1, "1")
+                            .addToBackStack("1")
+                            .commit();
                 } else {
                     mFragmentTransaction.remove(mFragment1).commit();
                 }
@@ -52,7 +57,9 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Frag
             }
             case R.id.second_fragment: {
                 if (item.isChecked()) {
-                    mFragmentTransaction.add(R.id.container2, mFragment2).commit();
+                    mFragmentTransaction.add(R.id.container2, mFragment2, "2")
+                            .addToBackStack("2")
+                            .commit();
                 } else {
                     mFragmentTransaction.remove(mFragment2).commit();
                 }
@@ -60,7 +67,9 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Frag
             }
             case R.id.third_fragment: {
                 if (item.isChecked()) {
-                    mFragmentTransaction.add(R.id.container3, mFragment3).commit();
+                    mFragmentTransaction.add(R.id.container3, mFragment3, "3")
+                            .addToBackStack("3")
+                            .commit();
                 } else {
                     mFragmentTransaction.remove(mFragment3).commit();
                 }
@@ -71,8 +80,16 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Frag
     }
 
     @Override
-    public boolean onClickContextItem(Fragment fragment) {
-        fragment.getView().setBackgroundColor(Utils.getColorRandom());
+    public boolean onClickContextItem(View view) {
+        final ArrayList<Integer> colorList = new ArrayList<>(3);
+        colorList.add(mFragment1.getColor());
+        colorList.add(mFragment2.getColor());
+        colorList.add(mFragment3.getColor());
+        int newColor = Utils.getColorRandom();
+        while (colorList.contains(newColor)) {
+            newColor = Utils.getColorRandom();
+        }
+        view.setBackgroundColor(newColor);
         return true;
     }
 }
--- app/src/main/java/org/avm/lesson8/MainFragment.java
+++ app/src/main/java/org/avm/lesson8/MainFragment.java
@@ -3,10 +3,9 @@ package org.avm.lesson8;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v4.app.Fragment;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,7 +13,7 @@ public class MainFragment extends Fragment {
 
     public interface FragmentListener {
 
-        boolean onClickContextItem(Fragment fragment);
+        boolean onClickContextItem(View fragment);
 
     }
 
@@ -31,24 +30,24 @@ public class MainFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.frafment, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment, container, false);
         mColor = Utils.getColorRandom();
         view.setBackgroundColor(mColor);
-        registerForContextMenu(view);
+        view.setOnClickListener(this::showBottomMenu);
         return view;
     }
 
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        menu.setHeaderTitle("Click for change color");
-        menu.setHeaderIcon(R.drawable.ic_action_name);
-        getActivity().getMenuInflater().inflate(R.menu.second_menu, menu);
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        return fragmentListener.onClickContextItem(this);
+    private void showBottomMenu(View view) {
+        View bottomMenuView = getLayoutInflater().inflate(R.layout.bottom_menu, null);
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getContext());
+        bottomSheetDialog.setContentView(bottomMenuView);
+        bottomMenuView.setOnClickListener(v -> {
+            fragmentListener.onClickContextItem(view);
+            bottomSheetDialog.dismiss();
+        });
+        bottomSheetDialog.show();
     }
 }
